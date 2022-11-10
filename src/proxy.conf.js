e047898733981f79cbf9f66db65bc64376073124
SaiSiddharth-vrna <117346308+SaiSiddharth-vrna@users.noreply.github.com>
2022-11-10 16:59:29 +0530

    Add files via upload

---
+++ src/proxy.conf.js
@@ -0,0 +1,39 @@
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/content-service",
+      "/auth-service", 
+      "/vrnaflow",
+      "/aws-service",
+    ],
+    target: "http://ec2-3-21-205-116.us-east-2.compute.amazonaws.com:8089",
+    secure: false,
+    logLevel: "debug",
+    changeOrigin: true,
+  },
+  {
+    context: [
+      "/vrna-prod-image-content"
+    ],
+    target: "https://s3.ap-south-1.amazonaws.com",
+    secure: false,
+    logLevel: "debug",
+    changeOrigin: true,
+  },
+  {
+    context: [
+      "/backdrops",
+      "/posters",
+    ],
+    target: "http://ec2-3-21-205-116.us-east-2.compute.amazonaws.com:8089",
+    secure: false, //8089 port
+    logLevel: "debug",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/backdrops": "/images/backdrops/",
+      "^/posters": "/images/posters/",
+    },
+  },
+];
+
+module.exports = PROXY_CONFIG;
