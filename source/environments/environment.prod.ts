27c55a38739c9530a4dc96e7e2ee755f6174a36a
Saisiddhp9 <117346308+SaiSiddharth-vrna@users.noreply.github.com>
2022-11-14 10:30:49 +0530

    assest and environments folders upload

---
+++ source/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+
+const apiUrl = 'https://ec2-3-21-205-116.us-east-2.compute.amazonaws.com:8089/orch-service';
+export const environment = {
+  production: true,
+  baseUrl: apiUrl,
+  contentUrl: '/content-service/',
+  vrnaFlowUrl: '/vrnaflow/',
+  awsUrl: '/aws-service/aws/',
+  s3Url: 'https://s3.ap-south-1.amazonaws.com'
+};
+
---
+++ source/environments/environment.ts
@@ -0,0 +1,10 @@
+const apiUrl = 'https://ec2-3-21-205-116.us-east-2.compute.amazonaws.com:8079/orch-service';
+export const environment = {
+  production: false,
+  baseUrl: apiUrl,
+  contentUrl: '/content-service/',
+  vrnaFlowUrl: '/vrnaflow/',
+  awsUrl: '/aws-service/aws/',
+  s3Url: 'https://s3.ap-south-1.amazonaws.com',
+ 
+};
